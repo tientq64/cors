1648e905d73aa7163ef67326bb9eba1aba4f52a9
tientq64 <tientq64@gmail.com>
2023-11-21 13:17:09 +0700

    ok!

--- api/get.js
+++ api/get.js
@@ -5,19 +5,10 @@ module.exports = async (req, res) => {
 
 	url = decodeURIComponent(url);
 
-	// let data = await (await fetch(url))[type]();
+	let data = await (await fetch(url))[type]();
 
 	res.setHeader("Access-Control-Allow-Credentials", true);
 	res.setHeader("Access-Control-Allow-Origin", "*");
 
-	// if (type !== "json") type = "send";
-
-	// res[type](data);
-
-	res.write("1. ");
-	res.write(type);
-	res.write("\n2. ");
-	res.write(url);
-	res.write("\n3. ");
-	res.end(req.query.url);
+	res.end(data);
 };
