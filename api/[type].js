c67595c883a04319087696c85c5e3e594dac9b55
tientq64 <tientq64@gmail.com>
2023-11-21 12:25:45 +0700

    aa

--- api/[type].js
+++ api/[type].js
@@ -3,7 +3,9 @@ const fetch = require("node-fetch");
 module.exports = async (req, res) => {
 	let { type, url } = req.query;
 
+	console.log(type, url, 1);
 	url = decodeURIComponent(url);
+	console.log(type, url, 2);
 
 	let data = await (await fetch(url))[type]();
 
