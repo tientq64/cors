6b240f7b808717e12f7a2fd14be9f72b82d0d2c5
tientq64 <tientq64@gmail.com>
2023-11-21 12:23:14 +0700

    sad

--- api/[type].js
+++ api/[type].js
@@ -1,11 +1,11 @@
-var fetch = require("node-fetch");
+const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
-	var { type, url } = req.query;
+	let { type, url } = req.query;
 
 	url = decodeURIComponent(url);
 
-	var data = await (await fetch(url))[type]();
+	let data = await (await fetch(url))[type]();
 
 	res.setHeader("Access-Control-Allow-Credentials", true);
 	res.setHeader("Access-Control-Allow-Origin", "*");
