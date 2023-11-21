9789e3d38e83ef4359d78090c513e85245721067
tientq64 <tientq64@gmail.com>
2023-11-21 11:54:39 +0700

    decodeURIComponent

--- api/[type].js
+++ api/[type].js
@@ -1,10 +1,16 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
-	const {type, url} = req.query
-	let data = await (await fetch(url))[type]()
-	res.setHeader('Access-Control-Allow-Credentials', true)
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	if (type !== 'json') type = 'send'
-	res[type](data)
+	const { type, url } = req.query;
+
+	url = decodeURIComponent(url);
+
+	const data = await (await fetch(url))[type]();
+
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	res.setHeader("Access-Control-Allow-Origin", "*");
+
+	if (type !== "json") type = "send";
+
+	res[type](data);
 }
